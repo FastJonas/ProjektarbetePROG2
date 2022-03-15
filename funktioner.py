51a8e9542158638be0f576205ba26238fa2cf912
MacksLH <99483061+MacksLH@users.noreply.github.com>
2022-03-15 13:55:41 +0100

    Update funktioner.py

--- funktioner.py
+++ funktioner.py
@@ -1,7 +1,11 @@
-
+import sys
+import time
 def nytt_rum(room):
     print('\n' * 100)
-    print((f'You enter the {room.get_platsname()} and look around.').center(170))
+    if room.get_plats !=13:
+        print((f'You enter the {room.get_platsname()} and look around.').center(170))
+    else:
+        print("You stand in front of the front door")
     print('\n' * 1)
     print((room.get_info()).center(170))
     
@@ -27,14 +31,50 @@ def nytt_rum(room):
 
 
 def pick_up_object(location, pocket):
-    if location.get_object() != 'No objects':
-        if location.get_object() not in pocket:
-            print((f'You see a {location.get_object()}.').center(170))
-            choice_object = input(f'You have the following options:\n(1) Pick up {location.get_object()}\n(2) Do not pick up the {location.get_object()}.\nEnter your choice: ')
+    objektval=input("Do you want to search the room for any objects?")
+    if objektval=="1":
+        if location.get_object() != 'No objects':
+            if location.get_object() not in pocket:
+                print((f'You see a {location.get_object()}.').center(170))
+                choice_object = input(f'You have the following options:\n(1) Pick up {location.get_object()}\n(2) Do not pick up the {location.get_object()}.\nEnter your choice: ')
 
-            if choice_object == str(1):
-                föremål_ryggsäck = pocket.append(location.get_object())
-                return föremål_ryggsäck
-
+                if choice_object == str(1):
+                    föremål_ryggsäck = pocket.append(location.get_object())
+                    return föremål_ryggsäck
+                    
+            else:
+                return
+            
         else:
-            return
+            print("No objects were found.")
+           
+    elif objektval=="2":
+        return
+    else:
+        print("You did not enter any of the options... Try again:")
+
+
+def door(room,pocket):
+    if room.get_plats()==13:
+        print("\n"*15)
+        print(("The door has a padlock, you need to enter the correct sequence of numbers to GET OUT?").center(170))
+        print("\n"*3)
+        while True:
+            doorchoice=input("You have the following options:\n(1) Enter the code\n(2) Stop trying\n\nEnter your choice: ")
+            print("\n"*3)
+            if doorchoice=="1":
+                print("In your pockets there is")
+                for elem in pocket:
+                    print ("a",elem)
+                    print("\n")
+                code=input("Write the correct code(4 numbers):")
+                if code=="7241":
+                    print(("The door opened and you got out of the house!").center(170))
+                    sys.exit()
+                else: 
+                    print(("You entered the wrong code").center(170))
+                    time.sleep(2)
+            elif doorchoice=="2":
+                break
+            else:
+                print("You did not enter any of the options... Try again:")
