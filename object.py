7041e77352a75377a60c4638ea528979cc7b9a39
edvinhellstrom <99483210+edvinhellstrom@users.noreply.github.com>
2022-03-17 11:38:21 +0100

    Update object.py

--- object.py
+++ object.py
@@ -11,7 +11,7 @@ def pick_up_object(location, pocket):
 
                 if choice_object == str(1):
                     object_pocket = pocket.append(location.get_object())
-                    if location.get_object() == 'Staircasekey':
+                    if location.get_object() == 'Staircasekey' or location.get_object == 'Officekey':
                         rootwindow = turtle.getcanvas().winfo_toplevel()
                         rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
                         rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
@@ -113,10 +113,251 @@ def pick_up_object(location, pocket):
                         t.write("woooow you found a key!",font=("Bauhaus 93",20,"normal"))
                         t.hideturtle()
                         turtle.exitonclick()
+
+                    elif location.get_object() == 'Note with number _ _ _ 1':
+                        rootwindow = turtle.getcanvas().winfo_toplevel()
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
+                        turtle.tracer(False)
+                        t = turtle.Turtle()
+                        t.hideturtle()
+                        t.speed(0)
+
+                        # Pappret
+                        t.goto(-200, -170)
+                        t.begin_fill()
+                        t.fillcolor('beige')
+                        t.width(5)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.lt(90)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.end_fill()
+
+                        # Siffra
+                        t.penup()
+                        t.goto(0, 0)
+                        t.pendown()
+                        t.write(1, font=('arial', 100), align='center')
+
+                        # Hålen
+                        k = 0; n = 10; y = 355
+                        while k < n:
+                            k += 1; y -= 50
+                            t.penup()
+                            t.goto(-190, y)
+                            t.pendown()
+                            t.circle(5)
+
+                        # lines
+                        k = 0; n = 24; y = 330
+                        while k < n:
+                            k += 1; y -= 20
+                            t.width(1)
+                            t.penup()
+                            t.goto(-197, y)
+                            t.pendown()
+                            t.lt(90)
+                            t.fd(400)
+                            t.rt(90)
+
+                        # redline
+                        t.penup()
+                        t.goto(-165, 327)
+                        t.pendown()
+                        t.color('red')
+                        t.fd(495)
+                        t.color('black')
+
+                        turtle.exitonclick()
+
+                    elif location.get_object() == 'Note with number _ 2 _ _':
+                        rootwindow = turtle.getcanvas().winfo_toplevel()
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
+                        turtle.tracer(False)
+                        t = turtle.Turtle()
+                        t.hideturtle()
+                        t.speed(0)
+
+                        # Pappret
+                        t.goto(-200, -170)
+                        t.begin_fill()
+                        t.fillcolor('beige')
+                        t.width(5)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.lt(90)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.end_fill()
+
+                        # Siffra
+                        t.penup()
+                        t.goto(0, 0)
+                        t.pendown()
+                        t.write(2, font=('arial', 100), align='center')
+
+                        # Hålen
+                        k = 0; n = 10; y = 355
+                        while k < n:
+                            k += 1; y -= 50
+                            t.penup()
+                            t.goto(-190, y)
+                            t.pendown()
+                            t.circle(5)
+
+                        # lines
+                        k = 0; n = 24; y = 330
+                        while k < n:
+                            k += 1; y -= 20
+                            t.width(1)
+                            t.penup()
+                            t.goto(-197, y)
+                            t.pendown()
+                            t.lt(90)
+                            t.fd(400)
+                            t.rt(90)
+
+                        # redline
+                        t.penup()
+                        t.goto(-165, 327)
+                        t.pendown()
+                        t.color('red')
+                        t.fd(495)
+                        t.color('black')
+
+                        turtle.exitonclick()
+
+                    if location.get_object() == 'Note with number _ _ 4 _':
+                        rootwindow = turtle.getcanvas().winfo_toplevel()
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
+                        turtle.tracer(False)
+                        t = turtle.Turtle()
+                        t.hideturtle()
+                        t.speed(0)
+
+                        # Pappret
+                        t.goto(-200, -170)
+                        t.begin_fill()
+                        t.fillcolor('beige')
+                        t.width(5)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.lt(90)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.end_fill()
+
+                        # Siffra
+                        t.penup()
+                        t.goto(0, 0)
+                        t.pendown()
+                        t.write(4, font=('arial', 100), align='center')
+
+                        # Hålen
+                        k = 0; n = 10; y = 355
+                        while k < n:
+                            k += 1; y -= 50
+                            t.penup()
+                            t.goto(-190, y)
+                            t.pendown()
+                            t.circle(5)
+
+                        # lines
+                        k = 0; n = 24; y = 330
+                        while k < n:
+                            k += 1; y -= 20
+                            t.width(1)
+                            t.penup()
+                            t.goto(-197, y)
+                            t.pendown()
+                            t.lt(90)
+                            t.fd(400)
+                            t.rt(90)
+
+                        # redline
+                        t.penup()
+                        t.goto(-165, 327)
+                        t.pendown()
+                        t.color('red')
+                        t.fd(495)
+                        t.color('black')
+
+                        turtle.exitonclick()
+
+                    elif location.get_object() == 'Note with number 7 _ _ _':
+                        rootwindow = turtle.getcanvas().winfo_toplevel()
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
+                        rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
+                        turtle.tracer(False)
+                        t = turtle.Turtle()
+                        t.hideturtle()
+                        t.speed(0)
+
+                        # Pappret
+                        t.goto(-200, -170)
+                        t.begin_fill()
+                        t.fillcolor('beige')
+                        t.width(5)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.lt(90)
+                        t.fd(400)
+                        t.lt(90)
+                        t.fd(500)
+                        t.end_fill()
+
+                        # Siffra
+                        t.penup()
+                        t.goto(0, 0)
+                        t.pendown()
+                        t.write(7, font=('arial', 100), align='center')
+
+                        # Hålen
+                        k = 0; n = 10; y = 355
+                        while k < n:
+                            k += 1; y -= 50
+                            t.penup()
+                            t.goto(-190, y)
+                            t.pendown()
+                            t.circle(5)
+
+                        # lines
+                        k = 0; n = 24; y = 330
+                        while k < n:
+                            k += 1; y -= 20
+                            t.width(1)
+                            t.penup()
+                            t.goto(-197, y)
+                            t.pendown()
+                            t.lt(90)
+                            t.fd(400)
+                            t.rt(90)
+
+                        # redline
+                        t.penup()
+                        t.goto(-165, 327)
+                        t.pendown()
+                        t.color('red')
+                        t.fd(495)
+                        t.color('black')
+
+                        turtle.exitonclick()
+
+
                     print('\n' * 50)
                     print((f'You pick up the {location.get_object()} and put it in your pocket.').center(170))
                     print((f'You are still in the {location.get_platsname()}.').center(170))
-
                     return object_pocket
                     
                 elif choice_object == str(2):
